a7363ff0d193de1ab9167bebcc771b043ab75c82
Hubert Ekvall <hubertekvall@gmail.com>
2023-03-27 17:25:06 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import './assets/main.css'
-import interact from interact
+import interact from 'interactjs'
 
 
 const app = createApp(App)
@@ -12,39 +12,65 @@ app.mount('#app')
 
 
 
+// target elements with the "draggable" class
+interact('#testDiv')
+  .draggable({
+    inertia: true,
+    autoScroll: true,
 
-makeDraggable(document.getElementById("testDiv"));
+    listeners: {
+     
+      move: dragMoveListener,
 
 
-function makeDraggable(element) {
-    let deltaMouse = {x: 0, y: 0};
-    let previousMouse = {x:0, y: 0};
+    }
+  })
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+// makeDraggable(document.getElementById("testDiv"));
+
+
+// function makeDraggable(element) {
+//     let deltaMouse = {x: 0, y: 0};
+//     let previousMouse = {x:0, y: 0};
    
-    element.onmousedown = onMouseDown;
+//     element.onmousedown = onMouseDown;
 
-    function onMouseDown (e)  {
-      e = e || window.event;
-      e.preventDefault();
+//     function onMouseDown (e)  {
+//       e = e || window.event;
+//       e.preventDefault();
       
-      previousMouse = {x: e.clientX, y: e.clientY};
+//       previousMouse = {x: e.clientX, y: e.clientY};
 
-      document.onmouseup = onMouseUp;
-      document.onmousemove = onMouseMove;
-    }
+//       document.onmouseup = onMouseUp;
+//       document.onmousemove = onMouseMove;
+//     }
   
-    function onMouseMove (e)  {
-      e = e || window.event;
-      e.preventDefault();
+//     function onMouseMove (e)  {
+//       e = e || window.event;
+//       e.preventDefault();
       
-      deltaMouse = {x: previousMouse.x - e.clientX, y: previousMouse.y  - e.clientY };
-      previousMouse = {x: e.clientX, y: e.clientY};
+//       deltaMouse = {x: previousMouse.x - e.clientX, y: previousMouse.y  - e.clientY };
+//       previousMouse = {x: e.clientX, y: e.clientY};
     
-      element.style.top = (element.offsetTop - deltaMouse.y) + "px";
-      element.style.left = (element.offsetLeft - deltaMouse.x) + "px";
-    }
+//       element.style.top = (element.offsetTop - deltaMouse.y) + "px";
+//       element.style.left = (element.offsetLeft - deltaMouse.x) + "px";
+//     }
   
-    function onMouseUp () {
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
-  }
+//     function onMouseUp () {
+//       document.onmouseup = null;
+//       document.onmousemove = null;
+//     }
+//   }
