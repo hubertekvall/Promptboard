8dbddf095d02ce48f8bd68c676b37b44c6a762a4
Hubert Ekvall <hubertekvall@gmail.com>
2023-04-21 14:33:00 +0200

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,50 @@ import interact from 'interactjs'
 import { Configuration, OpenAIApi } from 'openai';
 import PromptControl from './components/PromptControl.vue';
 
-let id = 0;
 
+let id = 0;
 
 
 export default {
 
     data() {
+        const self = this;
+
         return {
             message: "Hello world!",
             apiKey: '',
             prompts: {},
-            canvas: {}
+            canvas: {},
+
+            processMethods: {
+                'Persona': {
+                    method: async function (prompt) {
+                        const personaText = await self.getChatCompletion(prompt, `Generate a persona that is ${prompt.promptModifier}, be terse and short. Only include name, age, occupation, background, goals and motivations, interests and challenges. Output each header as an html list , put each header in a <strong> tag`);
+                        const [url, imagePrompt] = await self.getImageGeneration(prompt, "Generate an image prompt for this persona");
+
+                        console.log(prompt.messages);
+
+                        prompt.imagePrompt = imagePrompt;
+                        prompt.imageURL = url;
+                        prompt.response = personaText;
+                    },
+                  
+
+                },
+
+                'Brainstorm': function () {
+
+                },
+
+                'How might we': function () {
+
+                },
+
+                'Assistant persona': function () {
+
+                },
+            },
+            dragging: false
         }
     },
     components: {
@@ -24,7 +56,42 @@ export default {
 
 
     methods: {
+        async getChatCompletion(prompt, prefix) {
+            const openai = this.getOpenAIConfiguration();
 
+            if (prefix) {
+                prompt.messages.push({
+                    role: "user", content: prefix
+                });
+            }
+
+            const completion = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: prompt.messages,
+            });
+
+            const response = completion.data.choices[0].message.content
+            prompt.messages.push({ role: "assistant", content: response });
+            return response;
+        },
+
+        async getImageGeneration(prompt, prefix) {
+            const openai = this.getOpenAIConfiguration();
+
+            if (prefix) {
+                await this.getChatCompletion(prompt, prefix);
+            }
+
+            const imagePrompt = prompt.messages.at(-1).content;
+
+            const completion = await openai.createImage({
+                prompt: prompt.messages.at(-1).content,
+                n: 1,
+                size: "256x256",
+            });
+
+            return [completion.data.data[0].url, imagePrompt];
+        },
 
         refreshCanvas() {
 
@@ -34,8 +101,6 @@ export default {
         },
 
         getOffset(el) {
-
-
             const rect = el.getBoundingClientRect();
             return {
                 left: rect.left + window.scrollX,
@@ -63,7 +128,7 @@ export default {
 
             interact('.prompt').draggable({
                 autoScroll: true,
-                ignoreFrom: '.content',
+                ignoreFrom: '.prompt-inner',
                 cursorChecker: () => null,
                 listeners: {
 
@@ -78,16 +143,30 @@ export default {
                 listeners: {
                     end: (event) => {
                         this.refreshCanvas();
-
+                        this.dragging = false;
                     },
                     move: this.onDragConnector
                 }
             });
 
+
+            interact('.input-node').draggable({
+                autoScroll: true,
+
+                cursorChecker: () => null,
+                listeners: {
+                    end: (event) => {
+                        this.onRemoveConnector(event);
+                        this.refreshCanvas();
+                    },
+                }
+            });
+
+
             interact('.input-node').dropzone({
                 accept: '.output-node',
                 ondrop: this.onConnectPrompt
-            })
+            });
 
         },
 
@@ -100,7 +179,19 @@ export default {
             let startX = 200 * Math.random();
             let startY = 200 * Math.random();
             let id = uuidv4();
-            this.prompts[id] = { id: id, type: type, x: startX, y: startY, isDone: false, isProcessing: false, to: {}, from: {} }
+            this.prompts[id] = {
+                id: id,
+                type: type,
+                process: this.processMethods[type].method,
+                promptText: this.processMethods[type].promptText,
+                x: startX,
+                y: startY,
+                isDone: false,
+                isProcessing: false,
+                messages: [],
+                to: [],
+                from: null
+            }
         },
 
         onDragPrompt(event) {
@@ -117,12 +208,9 @@ export default {
         },
 
         onDragConnector(event) {
+            this.dragging = true;
             let promptID = event.target.parentNode.id;
             let prompt = this.prompts[promptID];
-
-            this.disconnectOutput(prompt);
-
-
 
             this.refreshCanvas();
             let ctx = this.canvas.getContext("2d");
@@ -133,7 +221,6 @@ export default {
 
             ctx.beginPath();
 
-
             ctx.moveTo(x, y);
             ctx.lineTo(event.pageX, event.pageY);
             ctx.lineWidth = 10.0;
@@ -143,165 +230,143 @@ export default {
             ctx.closePath();
         },
 
-
-        isNotEmpty(obj) {
-            return Object.keys(obj).length > 0
-        },
-
-        disconnectInput(prompt) {
-
-            prompt.from.to = {};
-            prompt.from = {};
-        },
-
-        disconnectOutput(prompt) {
-
-            prompt.to.from = {};
-            prompt.to = {};
-        },
-
-
         onConnectPrompt(event) {
             let senderPrompt = this.prompts[event.relatedTarget.parentNode.id];
             let receiverPrompt = this.prompts[event.target.parentNode.id];
 
 
+            // Walk the chain
             let startPrompt = senderPrompt;
-            while (this.isNotEmpty(startPrompt.from)) {
+            while (startPrompt.from !== null) {
                 startPrompt = startPrompt.from;
             }
 
-            if (senderPrompt.id !== receiverPrompt.id) {
-                if (startPrompt.id !== receiverPrompt.id) {
-                    this.disconnectOutput(senderPrompt);
-                    this.disconnectInput(receiverPrompt);
 
-                    senderPrompt.to = receiverPrompt
-                    receiverPrompt.from = senderPrompt
-                }
+            // Can't connect to a cycle or the same prompt
+            if (senderPrompt.id !== receiverPrompt.id && startPrompt.id !== receiverPrompt.id) {
+                senderPrompt.to.push(receiverPrompt);
+                receiverPrompt.from = senderPrompt
             }
         },
+        onRemoveConnector(event) {
+            let inputNode = this.prompts[event.target.parentNode.id];
+
+            if (inputNode.from !== null) {
+                this.removeConnector(inputNode.from, inputNode);
+            }
+        },
+
+
+        disconnectInput(prompt) {
+            if (prompt.from !== null) {
+                this.removeConnector(prompt.from, prompt);
+            }
+        },
+
+        disconnectOutputs(prompt) {
+            prompt.to.forEach(element => {
+                element.from = null;
+            });
+
+            prompt.to = [];
+        },
+        removeConnector(from, to) {
+            from.to = from.to.filter((p) => p.id !== to.id);
+            to.from = null;
+        },
+
+
+
+
+
 
 
         deletePrompt(promptID) {
             let prompt = this.prompts[promptID];
             this.disconnectInput(prompt);
-            this.disconnectOutput(prompt);
+            this.disconnectOutputs(prompt);
 
             delete this.prompts[promptID];
         },
+
+
+
         toggleEdit(promptID) {
             let prompt = this.prompts[promptID];
             prompt.isProcessing = false;
             prompt.isDone = false;
         },
 
-        async processPrompt(promptID, messages) {
+
+
+
+
+
+        getOpenAIConfiguration() {
+            const configuration = new Configuration({ apiKey: this.apiKey });
+            delete configuration.baseOptions.headers['User-Agent'];
+            return new OpenAIApi(configuration);
+        },
+
+
+
+
+
+        async processPrompt(promptID) {
             let prompt = this.prompts[promptID];
 
             prompt.isProcessing = true;
             prompt.isDone = false;
 
-            const configuration = new Configuration({ apiKey: this.apiKey });
-            delete configuration.baseOptions.headers['User-Agent'];
-            const openai = new OpenAIApi(configuration);
-
-
-            let promptMessages = messages ? [...messages] : [];
-            switch (prompt.type) {
-
-                case 'Fewshot':
-                    promptMessages.push(
-                        { role: "user", content: prompt.exampleTask },
-                        { role: "user", content: prompt.exampleAnswer },
-                        { role: "user", content: prompt.task }
-                    )
-                    break;
-
-                case 'Brainstorm':
-                    promptMessages.push({
-                        role: "user", content: prompt.promptText
-                    })
-
-                case 'Logotype':
-                    promptMessages.push(
-                        { role: "user", content: "A logotype that is" },
-                        { role: "user", content: prompt.promptText }
-                    )
-                    break;
-
-
+            if (prompt.from !== null) {
+                prompt.messages = from.messages.slice();
+            }
+            else {
+                prompt.messages = []
             }
 
-            
+            console.log(prompt.messages);
 
-
-            switch (prompt.type) {
-                case 'Fewshot':
-                case 'Brainstorm': {
-                    const completion = await openai.createChatCompletion({
-                        model: "gpt-3.5-turbo",
-                        messages: promptMessages,
-                    });
-
-                    prompt.response = completion.data.choices[0].message.content;
-                    promptMessages.push({
-                        role: "assistant", content: prompt.response
-                    })
-                }
-                    break;
-
-                case 'Icon':
-                case 'Logotype': {
-
-                    let merged = promptMessages.flatMap(pm => pm.message).join(' ');
-
-
-                    const completion = await openai.createImage({
-                        prompt: merged,
-                        n: 1,
-                        size: "256x256",
-                    });
-
-                    prompt.response = completion.data.data[0].url;
-                    prompt.isImage = true;
-                }
-
-                    break;
-            }
-
-
-
-
+            await prompt.process(prompt);
 
 
             prompt.isProcessing = false;
             prompt.isDone = true;
 
 
-            if (prompt.to.id in this.prompts) {
-                this.processPrompt(prompt.to.id, promptMessages);
-            }
+            prompt.to.forEach(element => {
+                this.processPrompt(element.id);
+            });
         },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         drawConnections() {
 
             const ctx = this.canvas.getContext("2d");
             for (const [key, value] of Object.entries(this.prompts)) {
 
-                if (this.isNotEmpty(value.to)) {
 
-                    let outputNode = this.getOffset(
-                        document.getElementById('output-' + key)
-                    );
-
+                let outputNode = this.getOffset(
+                    document.getElementById('output-' + key)
+                );
+                value.to.forEach(element => {
                     let inputNode = this.getOffset(
-                        document.getElementById('input-' + value.to.id)
+                        document.getElementById('input-' + element.id)
                     );
-
-
-
-
 
                     let y = (outputNode.top + outputNode.bottom) / 2;
                     let x = (outputNode.left + outputNode.right) / 2;
@@ -316,8 +381,7 @@ export default {
                     ctx.strokeStyle = "lightblue"
                     ctx.stroke()
                     ctx.closePath();
-                }
-
+                });
             }
 
         },
@@ -333,8 +397,11 @@ export default {
     },
     mounted: function () {
         this.initialize();
-    }
-    ,
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const key = urlParams.get('apiKey');
+        this.apiKey = key;
+    },
 
     updated: function () {
         this.retranslatePrompts();
@@ -355,47 +422,59 @@ export default {
     <div v-for="prompt in prompts">
 
         <div :id="prompt.id" :key="prompt.id"
-            class="prompt absolute top-0 left-0 w-96 bg-white max-w-xs p-4 shadow-xl rounded-xl flex flex-col">
-            <div :id="'input-' + prompt.id"
-                class="input-node absolute top-3 -left-6 rounded-full bg-gradient-to-t shadow-lg from-green-500 to-green-200 w-8 h-8">
+            class="prompt absolute top-0 left-0 max-w-xl bg-white p-4 shadow-xl rounded-xl flex flex-col">
+            <div :class="[dragging || prompt.from !== null ? 'bg-green-500' : 'bg-slate-400']" :id="'input-' + prompt.id"
+                class="input-node transition-all duration-500 absolute top-3 -left-6 rounded-full shadow-lg  w-8 h-8">
 
             </div>
             <div :id="'output-' + prompt.id"
-                class="output-node absolute top-3 -right-6 rounded-full bg-gradient-to-t shadow-lg from-blue-500 to-cyan-500 w-8 h-8 bg-red-500">
-
+                class="output-node absolute top-3 -right-6 rounded-full shadow-lg bg-blue-500 to-blue-400 w-8 h-8">
             </div>
 
             <h1 class="prompt-title text-xl font-extrabold ">
                 {{ prompt.type }}
             </h1>
 
+            <div class="prompt-inner transition-all duration-500">
+                <div v-if="prompt.isProcessing" class="h-32 justify-center items-center flex">
+                    <i class="gg-spinner"></i>
 
-            <div v-if="prompt.isProcessing" class="h-32 justify-center items-center flex">
-                <i class="gg-spinner"></i>
-            </div>
-
-            <div class="mt-4" v-else-if="prompt.isDone">
-                <img v-if="prompt.isImage" class="w-full shadow-md rounded-2xl" :src="prompt.response" />
-                <p v-else>{{ prompt.response }}</p>
-
-
-            </div>
-
-            <div v-else>
-                <div class="mt-4" v-if="prompt.type == 'Fewshot'">
-                    <input class="prompt-input" type="text" v-model="prompt.exampleTask" placeholder="Prompt" />
-                    <input class="prompt-input" type="text" v-model="prompt.exampleAnswer" placeholder="Prompt" />
-                    <input class="prompt-input" type="text" v-model="prompt.task" placeholder="Prompt" />
                 </div>
 
 
-                <div class="mt-4" v-else-if="prompt.type == 'Brainstorm'">
-                    <input class="prompt-input" type="text" v-model="prompt.promptText" placeholder="Prompt" />
-                </div>
 
-                <div class="mt-4" v-else-if="prompt.type == 'Logotype'">
-                    <input class="prompt-input" type="text" v-model="prompt.promptText"
-                        placeholder="Modern and sleek, bright colors" />
+                <div v-else class="transition-all">
+                    <div class="mt-4" v-if="prompt.isDone">
+                        <div v-if="prompt.type == 'Persona'" class="space-y-8  items-center flex flex-col">
+                            <img :src="prompt.imageURL" class="rounded-full shadow-lg" />
+                            <p class="text-xs w-96">“{{ prompt.imagePrompt }}”</p>
+
+                            <div v-html="prompt.response"
+                                class="overflow-y-scroll  space-y-2 max-h-64 text-sm rounded-xl bg-slate-50 p-4">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div v-else>
+                        <div class="w-96 mt-4" v-if="prompt.type == 'Persona'">
+
+                            <input class="prompt-input" type="text" v-model="prompt.promptModifier"
+                                placeholder="Describe something short that defines your persona" />
+
+                        </div>
+                    </div>
+
+
+
+                    <!-- <div class="mt-4" v-else-if="prompt.type == 'Brainstorm'">
+                                                                            <input class="prompt-input" type="text" v-model="prompt.promptText" placeholder="Prompt" />
+                                                                        </div>
+
+                                                                        <div class="mt-4" v-else-if="prompt.type == 'Logotype'">
+                                                                            <input class="prompt-input" type="text" v-model="prompt.promptText"
+                                                                                placeholder="Modern and sleek, bright colors" />
+                                                                        </div> -->
                 </div>
             </div>
 
@@ -411,22 +490,11 @@ export default {
 
     <div class="prompt-toolbar flex flex-wrap  sm:flex-row absolute bottom-10 left-10  ">
         <div class='flex  transition-all duration-500 bg-white p-1 sm:p-2 overflow-clip shadow-lg  rounded-xl space-x-2'>
-            <button @click="createPrompt('Fewshot')" class="px-3 rounded-md sm:rounded-lg py-1 hover:bg-green-200">
-                Fewshot </button>
-            <div class="bg-slate-200 w-px "> </div>
-
-            <button @click="createPrompt('Brainstorm')" class="px-3 rounded-md sm:rounded-lg py-1 hover:bg-green-200">
-                Brainstorm </button>
-
-            <div class=" bg-slate-200 w-px "> </div>
-
-            <button @click="createPrompt('Logotype')" class="px-3 rounded-md sm:rounded-lg py-1 hover:bg-green-200">
-                Logotype </button>
-
-            <div class=" bg-slate-200 w-px "> </div>
-            <button @click="createPrompt('Icon')" class="px-3 rounded-md sm:rounded-lg py-1 hover:bg-green-200"> Icon
-            </button>
-
+            <div v-for="(value, key) in processMethods">
+                <button @click="createPrompt(key)" class="px-3 rounded-md sm:rounded-lg py-1 hover:bg-green-200">
+                    {{ key }} </button>
+                <div class="bg-slate-200 w-px "> </div>
+            </div>
         </div>
 
         <input v-model="apiKey"
